5727e03fe7bbfdd7d35cecee228b98875bc99cfe
Valiveti Sri Ram Kumar <96459445+valiveti7@users.noreply.github.com>
2022-03-25 16:47:23 +0530

    Add files via upload

---
+++ letvar.js
@@ -0,0 +1,19 @@
+let a=10;
+console.log('lettttttttttttt',a);
+if (true){
+    let a=20;
+    console.log('lettttttttttttt',a);
+}
+console.log('lettttttttttttt',a);
+
+//in let global,local variables differs
+
+var b=10;
+console.log('lettttttttttttt',b);
+if (true){
+    var b=20;
+    console.log('lettttttttttttt',b);
+}
+console.log('lettttttttttttt',b);
+
+//value of var no global ,local difference.
